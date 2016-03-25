625be9d1293befd1060c32504a51d3fcaf981eed
Henri Keinonen <ketturi.fox@gmail.com>
2016-03-25 12:37:11 +0200

    Added documentation
    Better comments in source code. Schematic and board layout as images.

--- Arduino source/main.cpp
+++ Arduino source/main.cpp
@@ -1,51 +1,44 @@
-<<<<<<< HEAD
-﻿/*Led matrix controller
- * 128x10 ((32x10)x4) led matrix
- * controlled with 4bit row select lines and 32bit line shift register
- * Ketturi 2016
- */
-
-#include <avr/pgmspace.h>
-=======
-/*Led matrix controller
-   128x10 ((32x10)x4) led matrix
-   controlled with 4bit row select lines and 32bit line shift register.
-   ESP8266 as MCU
-   Ketturi 2016
+/*Led matrix IoT controller
+  128x10 (four 32x10 linked modules) led matrix display
+  controlled with 4bit row select lines and 32bit line shift register.
+  ESP8266 (ESP-12F module) as internet enabled application processor
+  http://esp8266.github.io/Arduino/versions/2.1.0/doc/reference.html
+  Ketturi 2016
 */
 
 #include <pgmspace.h>
->>>>>>> refs/remotes/origin/ESP8266-IoT
 #include <SPI.h>
-#include "bitmap.h" //image data from kukkaconvert.exe by wuffe goes here, put in PROGMEM to save ram
-#include "hardware.h" //hardware definitions
+#include "bitmap.h" //Image data from kukkaconvert.exe by wuffe goes here, put in PROGMEM to save ram
+#include "hardware.h" //Hardware definitions
 
 //ESP8266 stuff
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
-#include <ArduinoOTA.h>
+#include <ArduinoOTA.h> //http://esp8266.github.io/Arduino/versions/2.1.0/doc/ota_updates/ota_updates.html
 
-//WifiManager
+//WiFiManager
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
-#include <WiFiManager.h>
+#include <WiFiManager.h> //https://github.com/tzapu/WiFiManager
 
-#define ANIMATION false //animation or scrolling
+#define ANIMATION true //animation or scrolling
 
 byte pwm = 50; //sets initial brightness
 const unsigned int rowOnTime = 1000; //how long one row is lit, uS
-const unsigned int framerate = 50; //milliseconds between frames, slows display down, as it runs around 140fps
+const unsigned int framerate = 5000; //milliseconds between frames, slows display down to readable speed
 unsigned long previousMillis = 0;
 int frame = 0;
 
 void setup() { //Set pins to outputs and inits other stuff
   pinMode(16, SPECIAL); //Set GPIO16 to default function, controls reset
 
-  //Important, do not remove or update over wifi wont work!
-  setWiFi(); //Start WiFiManager
+  //Important, do not remove these, or update over wifi wont work!
+  //*******************************
+  setWiFi();   //Start WiFiManager
   otaUpdate(); //Enable ota updates
-
+  //*******************************
+  
   pinMode(0, FUNCTION_0);
   pinMode(1, FUNCTION_3);
   pinMode(2, FUNCTION_0);
@@ -63,24 +56,19 @@ void setup() { //Set pins to outputs and inits other stuff
   for (int i = 0; i < 4; i++) {
     pinMode(multiplexPin[i], OUTPUT);
   }
-<<<<<<< HEAD
-  
-  Serial.begin(9600);
-=======
 
->>>>>>> refs/remotes/origin/ESP8266-IoT
-  analogWrite(pwmPin, pwm);
+  analogWrite(pwmPin, pwm); //Set initial brightness
   SPI.begin();
-  Serial.setDebugOutput(0); //Stop all serial communication as serial pins are used for display
+  Serial.setDebugOutput(false); //Stop all serial communication as serial pins are used for display
   Serial.end();
 }
 
 void loop() {
-  ArduinoOTA.handle(); //Checks if incoming update
+  ArduinoOTA.handle(); //Checks for incoming update
 
-#if (ANIMATION)
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= framerate) {
+#if (ANIMATION) //shows single frames crating animation
+  unsigned long currentMillis = millis(); 
+  if (currentMillis - previousMillis >= framerate) { //crude timer for frame rate.
     previousMillis = currentMillis;
     if (frame < (imageWidth / 128 ))
       frame++;
@@ -93,7 +81,7 @@ void loop() {
     refreshDisplay(frame * 128, row);
   }
 
-#else
+#else //shows image that is scrolling from right to left
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= framerate) {
     previousMillis = currentMillis;
@@ -107,11 +95,10 @@ void loop() {
     multiplexRow(row);
     refreshDisplay(frame, row);
   }
-
 #endif
 }
 
-void refreshDisplay(int col, byte row) {
+void refreshDisplay(int col, byte row) { //Toggles control lines and outputs display data.
   digitalWrite(latchPin, LOW);
   shiftOut32(getRowWord(col + 96, row, imageWidth, imageData)); //last panel
   shiftOut32(getRowWord(col + 64, row, imageWidth, imageData)); //third panel
@@ -120,27 +107,24 @@ void refreshDisplay(int col, byte row) {
   digitalWrite(latchPin, HIGH);
 
   digitalWrite(enablePin, LOW);
-  /*Delay for display being on, affects how bright display is. Something smart with interrupts could be done here.
-    This is kinda critical as stuck leds may burn out at high brightness. Some kind of watchdoge to prevent this?
-  */
+  /*Delay for leds being enabled, affects how bright display is.
+    ESP8266 does WiFi and TCP/IP task during delays*/
   delayMicroseconds(rowOnTime);
   digitalWrite(enablePin, HIGH);
 }
 
 void multiplexRow(byte multiRow) { //Selects which row gets illuminated, 0-11 in four bits, 1-10 usable as display
   for (int b = 0; b < 4; b++) {
-    if ((0x01 & multiRow) < 0x01) {
+    if ((0x01 & multiRow) < 0x01)
       digitalWrite(multiplexPin[b], LOW);
-    }
-    else {
+    else
       digitalWrite(multiplexPin[b], HIGH);
-    }
     multiRow >>= 1;
   }
 }
 
-void shiftOut32(unsigned long input) { //32Bit shift out, sends data in 8bit pieces to shift register, LSBFIRST
-  SPI.beginTransaction(SPISettings(25000000, LSBFIRST, SPI_MODE0));
+void shiftOut32(unsigned long input) { //32Bit shift out with HW SPI, sends data in 8bit pieces to shift register
+  SPI.beginTransaction(SPISettings(25000000, LSBFIRST, SPI_MODE0)); //25Mhz, LSBFIRST, clock polarity and phase
   SPI.transfer(input >> 0);
   SPI.transfer(input >> 8);
   SPI.transfer(input >> 16);
@@ -148,9 +132,10 @@ void shiftOut32(unsigned long input) { //32Bit shift out, sends data in 8bit pie
   SPI.endTransaction();
 }
 
-const unsigned long getRowWord(const unsigned int x, unsigned const int y, const unsigned int width, const unsigned long* data) {  //getRowWord by wuffe, all cool stuff happens here
-  // Huomaa että x:n jakojäännökset saattavat olla hitaita, jos kääntäjä ei pysty mitenkään
-  // kääntäessä ennalta päättelemään, minkä arvon width saa funktiota kutsuttaessa.
+//getRowWord by wuffe, converts bitmap array to display lines
+const unsigned long getRowWord(const unsigned int x, unsigned const int y, const unsigned int width, const unsigned long* data) {
+  // NB: x's remainders may be slow to calculate, if compiler can't deduce beforehand
+  // what value "width" will get when function is called.
   const unsigned int xa = x % width;
   const unsigned int xb = (x + 32) % width;
   const unsigned int ya = y % 12;
@@ -159,12 +144,12 @@ const unsigned long getRowWord(const unsigned int x, unsigned const int y, const
   const unsigned int aPtr = ya + (xa / 32) * 12;
   const unsigned int bPtr = ya + (xb / 32) * 12;
 
-  // Konditionaali, ettei tulisi shiftattua 32:lla 32-bittistä muuttujaa (ei määritelty, ainakaan C:ssä).
+  //Conditional to prevent 32-bit variable being shifted with 32 (not defined, not in C anyhow).
   return shift == 0 ? data[aPtr] : ((data[aPtr] << shift) | (data[bPtr] >> (32 - shift)));
-  //return shift == 0 ? data + aPtr : (((data + aPtr) << shift) | ((data + bPtr) >> (32 - shift)));
+  //return shift == 0 ? pgm_read_dword(data + aPtr) : ((pgm_read_dword(data + aPtr) << shift) | (pgm_read_dword(data + bPtr) >> (bits - shift)));
 }
 
-void otaUpdate() { //Handless over the air updating
+void otaUpdate() { //Handless over the air updating from Arduino IDE
   WiFi.mode(WIFI_STA);
   WiFi.begin();
   ArduinoOTA.setHostname("FoxMatrix");
@@ -172,9 +157,8 @@ void otaUpdate() { //Handless over the air updating
   ArduinoOTA.begin();
 }
 
-void setWiFi() {
-  //If no wifi AP is found, create one, and serve user a config page at http://192.168.4.1
+void setWiFi() { //If no wifi AP is found, create one, and serve user a config page at http://192.168.4.1
   WiFiManager wifiManager;
-  wifiManager.setTimeout(180);
-  wifiManager.autoConnect("FoxMatrix");
+  wifiManager.setTimeout(180);          //In 3 minutes continue normaly even if no wlan detected.
+  wifiManager.autoConnect("FoxMatrix"); //Start WiFiManager with spesified AP name.
 }
